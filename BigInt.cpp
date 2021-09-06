980ce916c9684c7e3b7ec08c932cf10c5d19968b
Mohit Raghuwanshi <69432500+M0HiT27@users.noreply.github.com>
2021-09-06 09:49:01 +0530

    Add files via upload

---
+++ BigInt.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include "BigInt.h"
+using namespace std;
+//syntax to declare bigint == BigInt x;
+//bigint.read() [reads the value and stores it in the variable]
+//bigint.print() [outputs the value stored in the bigint]
+//bigint.setBig(string) [sets the value of bigint to passed string]
+
+//abs(bigint) [returns the abstract value of passed arg]
+//max(bigint , bigint) [returns the greater value ]
+//min(bigint , bigint) [returns the lesser value]
+//isequal(bigint , bigint ) [returns true if they are equal ]
+//isless(bigint , bigint) [returns true if first argument is less than second argument]
+//isgreater(bigint , bigint) [returns true if first argument is greater than second argument]
+//swap(bigint , bigint ) [swaps the value of passed args]
+//sort(bigint[] , size) [arguments=a bigint array + size of array]
+//add(BigInt x , BigInt y) [returns the sum]
+//substract(BigInt x , BigInt y)[return the substraction (x-y)]
+//multiply(BigInt x , BigInt y)[returns the product]
+//digitsum(BigInt x) [returns the sum of BigInt]
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+	freopen("input.txt", "r", stdin);
+
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+
+
+	return 0;
+}
+
